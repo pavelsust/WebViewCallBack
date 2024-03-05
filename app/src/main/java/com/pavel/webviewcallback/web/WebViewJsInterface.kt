d1738839c38ba329bdfdb569959119911c397abf
Masud  Parvez <masudparvez@Masuds-MacBook-Pro.local>
2024-03-05 23:11:43 +0600

    done

--- app/src/main/java/com/pavel/webviewcallback/web/WebViewJsInterface.kt
+++ app/src/main/java/com/pavel/webviewcallback/web/WebViewJsInterface.kt
@@ -19,9 +19,47 @@ class WebViewJsInterface(private val context: Context) {
     }
 
     @JavascriptInterface
-    fun eventExecute(params: String) {
+    fun eventExecute(params: String, value:String) {
         Toast.makeText(context, "event $params", Toast.LENGTH_LONG).show()
+        Toast.makeText(context, "value $value", Toast.LENGTH_LONG).show()
     }
-
 }
 
+
+// class WebViewJsInterface(private val context: Context) {
+//    @JavascriptInterface
+//    fun eventExecute(params: String?) {
+//
+//        //Logger.d("PartnerServiceDebugLog params: $params")
+//
+//        /*val bundle = Bundle()
+//
+//        if (!TextUtils.isEmpty(params)) {
+//
+//        val paramArray = params?.split(",")?.toTypedArray()
+//
+//        bundle.putString(paramArray?.getOrNull(0), paramArray?.getOrNull(1))
+//
+//        Application.logSDKEvent(partnerSlug, name, bundle)
+//
+//        }*/
+//
+//    }
+//    @JavascriptInterface
+//    fun tokenStatus(value: String?, message: String?) {
+//        show("token is ${value}")
+//        show("Message is ${message}")
+//    }
+//
+//    @JavascriptInterface
+//    fun contentChange(path: String?, isPremium: String?) {
+//        show("Path: ${path}")
+//        show("Premium: ${isPremium}")
+//    }
+//
+//    fun show(message: String){
+//        Toast.makeText(context , message , Toast.LENGTH_SHORT ).show()
+//    }
+//
+//}
+//
